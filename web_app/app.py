46a8c615866b2d25020cea5a99b2a0ba60e1aae5
Burke Morse <burkemorse@Burkes-MacBook-Pro.local>
2024-01-28 16:03:41 -0600

    basic user input in web_app

--- web_app/app.py
+++ web_app/app.py
@@ -8,13 +8,25 @@ app = Flask(__name__)
 def main():
     return '''
      <form action="/echo_user_input" method="POST">
-         <input name="user_input">
+         <label for="city_input">Enter City:</label>
+         <input id="timeframe_input" name="city" required>
+         <br>
+         <label for="timeframe_dropdown">Select Timeframe:</label>
+         <select id="timeframe_dropdown" name="selected_timeframe">
+             <option value="next week">Next Week</option>
+             <option value="next month">Next Month</option>
+             <option value="next 6 months">Next 6 Months</option>
+             <!-- Add more cities as needed -->
+         </select>
+         <br>
          <input type="submit" value="Submit!">
      </form>
      '''
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
-#poopy
+    city = request.form.get("city", "")
+    selected_timeframe = request.form.get("selected_timeframe", "")
+    return f"Searching for events in {city} in the {selected_timeframe}!"
+
+
